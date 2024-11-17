cd9267ee82b1554444e4588c8dd040f5e74eb4a9
aein <mananniaein1234@gmail.com>
2024-11-17 15:09:52 +0330

    add location

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import MyMap from "./components/map-components";
 import { MapProvider } from "./context/useContext";
+import Weather from "./components/weather-component";
 
 const App = () => {
   return (
     <MapProvider>
       <MyMap />
+      <Weather />
     </MapProvider>
   );
 };
--- src/components/weather-component/index.tsx
+++ src/components/weather-component/index.tsx
@@ -1,24 +1,35 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-
-const apikey = "3676WXHDCSPRVND4AR8NCFEAX";
+import { useMapContext } from "../../context/useContext";
+const apikey = import.meta.env.VITE_API_KEY;
 
 const Weather = () => {
+  const { location } = useMapContext();
   const [state, setState] = useState(null);
-
+  const latitude = parseFloat(location[0].toFixed(2));
+  const longitude = parseFloat(location[1].toFixed(2));
   const handelApiInfo = async () => {
     try {
-      const response = await axios.get(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/38.9697,-77.385?key=${apikey}`
-      );
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+      const response = await axios.get(url);
+      console.log(response.data);
       setState(response.data);
     } catch (error) {
-      console.log(error);
+      if (
+        location[0] < -90 ||
+        location[0] > 90 ||
+        location[1] < -180 ||
+        location[1] > 180
+      ) {
+        console.error("Invalid latitude or longitude values:", location, error);
+        return;
+      }
     }
   };
   useEffect(() => {
     handelApiInfo();
-  }, [state]);
+  }, [location]);
+  console.log(latitude);
   return (
     <div>
       <pre className="">{JSON.stringify(state, null, 2)}</pre>
